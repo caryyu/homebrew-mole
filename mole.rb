81597cffa8af5dbdb42732590887ba2aaf47ddaf
David Pinheiro <david.pinheiro@hpe.com>
2020-08-13 15:02:01 -0700

    mole version 1.0.0

--- mole.rb
+++ mole.rb
@@ -1,9 +1,9 @@
-MOLE_VERSION = "0.5.0"
-MOLE_SHA = "58cff1a00510eb677e0bf929393f9697e71ea25e3c8a84290e4a75d3b91f7648"
-GO_VERSION = "1.13.1"
+MOLE_VERSION = "1.0.0"
+MOLE_SHA = "7e98e21a8f450784d9f68bd05135470bffed645d84c7071de0c73a1276f42673"
+GO_VERSION = "1.14.7"
 
 class Mole < Formula
-  desc "App to create ssh tunnels"
+  desc "App to create ssh tunnels focused on resiliency and user experience."
   homepage "https://davrodpin.github.io/mole/"
   url "https://github.com/davrodpin/mole/archive/v#{MOLE_VERSION}.tar.gz"
   sha256 "#{MOLE_SHA}"
@@ -14,11 +14,11 @@ class Mole < Formula
     bin_path.install Dir["*"]
 
     cd bin_path do
-      system "go", "build", "-o", bin/"mole", "-ldflags", "-X main.version=#{MOLE_VERSION}", "github.com/davrodpin/mole/cmd/mole"
+      system "go", "build", "-o", "bin/mole", "-ldflags", "-X main.version=#{MOLE_VERSION}", "github.com/davrodpin/mole"
     end
   end
 
   test do
-    assert_match "mole #{MOLE_VERSION}", shell_output("#{bin}/mole -version 2>&1", 2)
+    assert_match "mole #{MOLE_VERSION}", shell_output("#{bin}/mole version 2>&1", 2)
   end
 end
