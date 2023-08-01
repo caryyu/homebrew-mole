e5677a6633781fb8454cbfaeff3781c8044c588e
ask me anything :) <caryyu.tg@gmail.com>
2023-08-01 14:27:43 +0800

    Update mole.rb

--- mole.rb
+++ mole.rb
@@ -1,6 +1,5 @@
 MOLE_VERSION = "2.0.0"
 MOLE_SHA = "adafcf59c0b0c0270e466b4d7e15f98c06641870dd70f0b275c53a3d52608b2d"
-GO_VERSION = "1.17.12"
 
 class Mole < Formula
   desc "App to create ssh tunnels focused on resiliency and user experience."
@@ -8,7 +7,7 @@ class Mole < Formula
   url "https://github.com/davrodpin/mole/archive/v#{MOLE_VERSION}.tar.gz"
   sha256 "#{MOLE_SHA}"
 
-  depends_on "go" => GO_VERSION
+  depends_on "go@1.17"
   def install
     bin_path = buildpath/"src/github.com/davrodpin/mole"
     bin_path.install Dir["*"]
